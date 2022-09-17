e3c77bb74e8b42a2ea0ceb4076bfe80dd068d234
guillermo-st <guillermo_stancanelli@outlook.com>
2022-09-17 13:18:41 -0300

    add parsing for the server start commands

---
+++ src/start-server.py
@@ -0,0 +1,26 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Start the fileserver and listen for clients.')
+
+exclusives = parser.add_mutually_exclusive_group()
+
+parser.add_argument('-h', '--help', action='store_true',
+                    help='show this help message and exit')
+
+exclusives.add_argument('-v', '--verbose', action='store_true',
+                    help='increase output verbosity')
+
+exclusives.add_argument('-q', '--quiet', action='store_true',
+                    help='decrease output verbosity')
+
+parser.add_argument('-H', '--host', action='store', nargs=1,
+                    help='specify the service IP address.')
+
+parser.add_argument('-p', '--port', action='store', nargs=1,
+                    help='specify the service port')
+
+parser.add_argument('-s', '--storage', action='store',nargs=1,
+                    help='specify the storage path')
+
+args = parser.parse_args()
+print(args)
