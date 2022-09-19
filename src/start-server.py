5ccaa3d950ef262b7af893822efcc2e2ee3876a0
Bruno Grassano <bgrassano@fi.uba.ar>
2022-09-18 21:09:14 -0300

    Join args with initial client-server

--- src/start-server.py
+++ src/start-server.py
@@ -1,30 +1,20 @@
 import argparse
-
-parser = argparse.ArgumentParser(description='Start the fileserver and listen for clients.')
-
-exclusives = parser.add_mutually_exclusive_group()
-
-exclusives.add_argument('-v', '--verbose', action='store_true',
-                    help='increase output verbosity')
-
-exclusives.add_argument('-q', '--quiet', action='store_true',
-                    help='decrease output verbosity')
-
-parser.add_argument('-H', '--host', action='store', nargs=1,
-                    help='specify the service IP address.')
-
-parser.add_argument('-p', '--port', action='store', nargs=1,
-                    help='specify the service port')
-
-parser.add_argument('-s', '--storage', action='store',nargs=1,
-                    help='specify the storage path')
-
-args = parser.parse_args()
-print(args)
-
 import threading
 import random
+import lib.constants as constants
 from socket import *
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Start the fileserver and listen for clients.')
+    exclusives = parser.add_mutually_exclusive_group()
+    exclusives.add_argument('-v', '--verbose', action='store_true',help='increase output verbosity')
+    exclusives.add_argument('-q', '--quiet', action='store_true',help='decrease output verbosity')
+    parser.add_argument('-H', '--host', action='store', nargs=1,default=[constants.DEFAULT_HOST],metavar="ADDR",help='specify the service IP address.')
+    parser.add_argument('-p', '--port', action='store', nargs=1, default=[constants.DEFAULT_PORT], type=int,help='specify the service port')
+    parser.add_argument('-s', '--storage', action='store',nargs=1,help='specify the storage path')
+    return parser.parse_args()
+
+
 
 def read_client(client_address):
     client_socket = socket(AF_INET, SOCK_DGRAM)
@@ -37,11 +27,10 @@ def read_client(client_address):
         modifiedMessage = "[SERVER]: " +message.upper()
         client_socket.sendto(modifiedMessage.encode(), clientAddress)
 
-def start_server():
+def start_server(server_name: str, server_port: int):
     clients = []
-    server_port = 12000
     welcoming_socket = socket(AF_INET, SOCK_DGRAM)
-    welcoming_socket.bind(("", server_port))
+    welcoming_socket.bind((server_name, server_port))
     print("The server is ready to receive")
     while True:
         message, clientAddress = welcoming_socket.recvfrom(2048)
@@ -49,10 +38,10 @@ def start_server():
         if message == b'close':
             break;
         clients.append(threading.Thread(target=read_client(clientAddress)).start())
-        welcoming_socket.sendto(("[SERVER]: " + modifiedMessage).encode(), clientAddress)
 
     for client in clients:
         client.join()
 
 if __name__ == '__main__':
-    start_server()
+    args = get_args()
+    start_server(args.host[0],args.port[0])
--- src/upload.py
+++ src/upload.py
@@ -1,32 +1,32 @@
 import argparse
-
-parser = argparse.ArgumentParser(description='< command description >')
-
-
-parser.add_argument('-v','--verbose', dest='verbose', action='store_true',help='increase output verbosity')
-    
-parser.add_argument('-q','--quiet', dest='quiet', action='store_true',help='decrease output verbosity')
-
-parser.add_argument('-H','--host',nargs=1, dest='host', action='store',help='server IP address')
-
-parser.add_argument('-p','--port',nargs=1, dest='port', action='store',help='server port')
-
-parser.add_argument('-s','--src',nargs=1, dest='src', action='store',help='source file path')
-
-parser.add_argument('-n','--name',nargs=1, dest='name', action='store',help='file name')
-
-args = parser.parse_args()
-print(args)
-
 from socket import *
-serverName = "localhost"
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-while True:
-    message = "[CLIENT]: " + input("Input lowercase sentence:")
-    if message == "q":
-        break;
-    clientSocket.sendto(message.encode(),(serverName, serverPort))
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    print(modifiedMessage.decode())
-clientSocket.close()
+import lib.constants as constants
+
+def get_args():
+    parser = argparse.ArgumentParser(description='< command description >')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="count",help="increase output verbosity")
+    group.add_argument("-q", "--quiet", action="count", help="decrease output verbosity")
+    parser.add_argument('-H','--host',nargs=1, dest='host', default=[constants.DEFAULT_HOST],metavar="ADDR", action='store',help='server IP address')
+    parser.add_argument('-p','--port',nargs=1, dest='port', type=int,default=[constants.DEFAULT_PORT],action='store',help='server port')
+    parser.add_argument('-s','--src',nargs=1, dest='src', metavar="FILEPATH",action='store',help='source file path')
+    parser.add_argument('-n','--name',nargs=1, dest='name', metavar="FILENAME",action='store',help='file name')
+    return parser.parse_args()
+
+
+def upload(server_name: str, server_port: int):
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    while True:
+        lowerCase = input("Input lowercase sentence:")
+        message = "[CLIENT]: " + lowerCase
+        if lowerCase == "q":
+            break;
+        clientSocket.sendto(message.encode(),(server_name, server_port))
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        print(modifiedMessage.decode())
+    clientSocket.close()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    upload(args.host[0],args.port[0])
