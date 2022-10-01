e682f21f84bd7bbc5838ac1169c6cb9fb0c59afd
guillermo-st <guillermo_stancanelli@outlook.com>
2022-10-01 20:40:12 -0300

    Add third handshake step

--- src/lib/protocols/base_protocol.py
+++ src/lib/protocols/base_protocol.py
@@ -33,9 +33,16 @@ class BaseProtocol:
                 self.socket.send(segment.as_bytes())
                 answer, address = self.socket.read(const.MSG_SIZE)
                 self.socket.setaddress(address)
-
                 segment = RDTPSegment.from_bytes(answer)
-                return not segment.header.fin, InitialMessage.from_bytes(segment.data).file_size
+                initial_msg_response = InitialMessage.from_bytes(segment.data)
+
+                if segment.header.fin:
+                    return False, initial_msg_response.file_size
+
+                third_handshake_step_header = RDTPHeader(0, 0, False)
+                third_handshake_step_segment = RDTPSegment(bytearray([]), third_handshake_step_header)
+                self.socket.send(third_handshake_step_segment.as_bytes())
+                return True, initial_msg_response.file_size
 
             except timeout:
                 attempts += 1
