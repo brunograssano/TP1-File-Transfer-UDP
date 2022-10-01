1faf6f635f5b8c0a1a53937d395e4f4579556e38
Bruno Grassano <bgrassano@fi.uba.ar>
2022-10-01 14:45:16 -0300

    Add required arg in name

--- src/lib/parser.py
+++ src/lib/parser.py
@@ -20,17 +20,16 @@ def client_parser(description):
     parser = common_parser(description)
     group_transfer = parser.add_mutually_exclusive_group()
     group_transfer.add_argument("-saw", "--stop_and_wait",action='store_true', help="choose Stop and Wait transfer")
-    group_transfer.add_argument("-gbn", "--go_back_n", metavar="GO_BACK_N",help="choose Go Back N transfer")
+    group_transfer.add_argument("-gbn", "--go_back_n", help="choose Go Back N transfer")
+    parser.add_argument('-n','--name',nargs=1, dest='name', required=True, metavar="FILENAME",action='store',help='file name')
     return parser    
 
 def upload_parser():
     parser = client_parser("description: Uploads a file to the server")
-    parser.add_argument('-s','--src',nargs=1, dest='src', metavar="FILEPATH",action='store',help='source file path')
-    parser.add_argument('-n','--name',nargs=1, dest='name', metavar="FILENAME",action='store',help='file name')
+    parser.add_argument('-s','--src',nargs=1, dest='src', default=[constants.DEFAULT_SRC], metavar="FILEPATH",action='store',help='source file path')
     return parser.parse_args()
 
 def download_parser():
     parser =  client_parser("description: Downloads a specific file from the server")
-    parser.add_argument("-d", "--dst", default=".",metavar="FILEPATH", help="destination file path")
-    parser.add_argument("-n", "--name", default="",metavar="FILENAME", help="file name")
+    parser.add_argument("-d", "--dst",nargs=1, dest='dst', default=[constants.DEFAULT_DST],metavar="FILEPATH", help="destination file path")
     return parser.parse_args()
